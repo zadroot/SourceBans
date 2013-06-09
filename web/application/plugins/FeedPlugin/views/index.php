58cf490f5c4a5b7588ed822335c27b6ef19d1679
Lane Babuder <flyingmongoose@gmail.com>
2013-06-09 15:52:31 -0400

    * Output Updates * Included (commented out for now) cross-browser compatibility for different feed types. * Now allowing -1 for feed_item_count, this allows all bans to be displayed.

--- web/application/plugins/FeedPlugin/views/index.php
+++ web/application/plugins/FeedPlugin/views/index.php
@@ -1,12 +1,31 @@
 
 <?php
-	header('Content-Type: application/rss+xml; charset=UTF-8');
-	for($i=0; $i < SourceBans::app()->settings->feed_item_count; $i++)
-	{
-		echo ($i);
+	// Checks to see if the requesting agent is a Mozilla type browser
+	// If it is, it pushes text/xml to enforce it to read the page as XML
+	// If it is not, it returns proper application response.
+	/* Section Disabled during development
+	if(strstr(Yii::app()->request->getUserAgent(),"Mozilla") == "FALSE")
+	{*/
+		header('Content-Type: application/rss+xml; charset=UTF-8');
+	/*}else{
+		header('Content-Type: text/xml; charset=UTF-8');
+	};
+	 */
 ?>
-test
+	<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
 <?php
+
+	$bans = $this->getBans();
+	if(SourceBans::app()->settings->feed_item_count < 0)
+	{
+		foreach($bans as $ban)
+		{
+			
+		}
+	}else{
+		for($i=0; $i < SourceBans::app()->settings->feed_item_count; $i++)
+		{
+		}
 	}
-	print_r ($this->getBans());
+	print_r ($bans);
 ?>
--- web/application/plugins/FeedPlugin/views/settings.php
+++ web/application/plugins/FeedPlugin/views/settings.php
@@ -9,4 +9,4 @@ In order to make use of this feed, you can use of the following (in order of rec
 <a href="<?php echo (Yii::app()->request->getBaseUrl(true)); ?>/feed.php" target="_blank">.php</a> 
 <br />
 <br />
-Current number of items displayed in the feed: <?php echo ($itemCount); ?>
+Current number of items displayed in the feed set to -1 for unlimited: <?php echo ($itemCount); ?>
